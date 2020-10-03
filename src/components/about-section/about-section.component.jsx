63c5f9419965aee7893a350e8395e4b56ac24e24
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-03 17:31:26 -0400

    add about section blurb

--- src/components/about-section/about-section.component.jsx
+++ src/components/about-section/about-section.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { Photo } from '../photo/photo.component';
+import { Blurb } from '../blurb/blurb.component';
+
+import './about-section.styles.css'
 
 export const About = () => {
     return (
         <div className="about">
             <Photo />
+            <Blurb />
         </div>
     )
 }
---
+++ src/components/blurb/blurb.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './blurb.styles.css';
+
+export const Blurb = () => {
+    return(
+        <div className="blurb">
+            <h1>Hi! I'm Kevin, a full stack web developer passionate about building secure and scalable applications.</h1>
+            <p>I primarily work on MERN stack projects, but I am also familiar with PHP and Ruby. I love learning new coding languages and technologies and am always eager to further develop my craft.  When I'm not writing code I enjoy tabletop gaming, discovering new music and spending time with my dog.</p>
+        </div>
+    )
+}
