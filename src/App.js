23fe54c0d5d56b8c9f8e11da2767dfd87db1f8e3
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-03 19:25:10 -0400

    add nav bar and set state to show about by default

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import { Nav } from './components/nav/nav.component';
 import { About } from './components/about-section/about-section.component';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component{
     super();
 
     this.state = {
+      currentSection: 'about' 
 
     }
 
@@ -16,7 +18,8 @@ class App extends Component{
   render(){
     return (
       <div>
-        <About />
+        <Nav />
+        { this.state.currentSection === 'about' && <About /> }
       </div>
     )
   }
---
+++ src/components/nav/nav.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './nav.styles.css';
+
+export const Nav = () => {
+    return (
+        <ul className="nav">
+            <li className="nav-item">About</li>
+            <li className="nav-item">Projects</li>
+            <li className="nav-item">Contact</li>
+            <li className="nav-item">Download Resume</li>
+        </ul>
+    )
+}
