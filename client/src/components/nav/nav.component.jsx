7f8f9e68a966f8154a38f51c1a8593f66599fc82
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-05 11:05:31 -0400

    can now get resume to download on client side

--- client/src/components/nav/nav.component.jsx
+++ client/src/components/nav/nav.component.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { saveAs } from 'file-saver';
+
 import './nav.styles.css';
 
 export const Nav = ({changeSection}) => {
-    const serveResume = () => {
-        fetch('http://localhost:5000/resume')
-            .catch(err => console.log(err));
+    const getResume = () => {
+        fetch('http://localhost:5000/resume', {
+            responseType: 'blob',
+            headers: {
+                Accept: 'application/pdf',
+            }
+        })
+        .then(response => {
+            console.log(response)
+            window.open(response.url)
+        })
+        .catch(err => console.log(err));
     }
 
     return (
@@ -13,7 +24,7 @@ export const Nav = ({changeSection}) => {
             <li className="nav-item" onClick={e => changeSection('about')}>About</li>
             <li className="nav-item" onClick={e => changeSection('projects')}>Projects</li>
             <li className="nav-item" onClick={e => changeSection('contact')}>Contact</li>
-            <li className="nav-item" onClick={() => serveResume()}>Download Resume</li>
+            <li className="nav-item" onClick={() => getResume()}>Download Resume</li>
         </ul>
     )
 }
