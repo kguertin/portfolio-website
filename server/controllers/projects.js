bd415f3fc7ea1689f63ce2732f1267c626f25830
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-04 23:33:15 -0400

    send resume to client side via node

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const path = require('path')
+
 exports.getResume = (req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
+    fs.readFile(path.join('data', 'resume.pdf'), (err, data) => {
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'inline; filename="' + 'resume.pdf' + '"');
+      console.log(res)
+      res.status(200).send(data);
     })
 }
