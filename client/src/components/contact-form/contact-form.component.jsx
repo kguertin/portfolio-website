ecb59241a813af571f12ea71e6ab3da2c622133c
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-10 17:19:29 -0400

    build form and form state to handle contact functionality

--- client/src/components/contact-form/contact-form.component.jsx
+++ client/src/components/contact-form/contact-form.component.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Component } from 'react';
 
 import './contact-form.styles.css';
 
-export const ContactForm = () => {
-    return (
-        <p>contact form</p>
-    )
+export class ContactForm extends Component{
+    constructor() {
+        super();
+
+        this.state = {
+            message: '',
+            contact: ''
+        }
+    }
+
+    handleContactChange = (event) => {
+        this.setState({contact: event.target.value})
+    }
+
+    handleMessageChange = (event) => {
+        this.setState({message: event.target.value})
+    }
+
+    render() {
+        return (
+           <div>
+                <p>{this.state.contact}</p>
+                <p>{this.state.message}</p>
+               <div className="form-section">
+                    <label>
+                        Contact Email: 
+                        <input type="email" value={this.state.contact} onChange={this.handleContactChange} />
+                    </label>
+               </div>
+               <div className="form-section">
+                    <textarea value={this.state.message} onChange={this.handleMessageChange} id="" cols="30" rows="10"></textarea>
+               </div>
+               <button>Submit</button>
+           </div>
+        )
+    }
 } 
